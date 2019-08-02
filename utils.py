0ce8f073f34ccd1a89122cb743dcb91bdf604b34
TakeruIto <39827302+TakeruIto@users.noreply.github.com>
2019-08-02 16:01:03 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -38,6 +38,7 @@ import time
 # get_scheduler
 # weights_init
 
+
 def get_all_data_loaders(conf):
     batch_size = conf['batch_size']
     num_workers = conf['num_workers']
@@ -51,49 +52,58 @@ def get_all_data_loaders(conf):
 
     if 'data_root' in conf:
         train_loader_a = get_data_loader_folder(os.path.join(conf['data_root'], 'trainA'), batch_size, True,
-                                              new_size_a, height, width, num_workers, True)
+                                                new_size_a, height, width, num_workers, True)
         test_loader_a = get_data_loader_folder(os.path.join(conf['data_root'], 'testA'), batch_size, False,
-                                             new_size_a, new_size_a, new_size_a, num_workers, True)
+                                               new_size_a, new_size_a, new_size_a, num_workers, True)
         train_loader_b = get_data_loader_folder(os.path.join(conf['data_root'], 'trainB'), batch_size, True,
-                                              new_size_b, height, width, num_workers, True)
+                                                new_size_b, height, width, num_workers, True)
         test_loader_b = get_data_loader_folder(os.path.join(conf['data_root'], 'testB'), batch_size, False,
-                                             new_size_b, new_size_b, new_size_b, num_workers, True)
+                                               new_size_b, new_size_b, new_size_b, num_workers, True)
     else:
         train_loader_a = get_data_loader_list(conf['data_folder_train_a'], conf['data_list_train_a'], batch_size, True,
-                                                new_size_a, height, width, num_workers, True)
+                                              new_size_a, height, width, num_workers, True)
         test_loader_a = get_data_loader_list(conf['data_folder_test_a'], conf['data_list_test_a'], batch_size, False,
-                                                new_size_a, new_size_a, new_size_a, num_workers, True)
+                                             new_size_a, new_size_a, new_size_a, num_workers, True)
         train_loader_b = get_data_loader_list(conf['data_folder_train_b'], conf['data_list_train_b'], batch_size, True,
-                                                new_size_b, height, width, num_workers, True)
+                                              new_size_b, height, width, num_workers, True)
         test_loader_b = get_data_loader_list(conf['data_folder_test_b'], conf['data_list_test_b'], batch_size, False,
-                                                new_size_b, new_size_b, new_size_b, num_workers, True)
+                                             new_size_b, new_size_b, new_size_b, num_workers, True)
     return train_loader_a, train_loader_b, test_loader_a, test_loader_b
 
 
 def get_data_loader_list(root, file_list, batch_size, train, new_size=None,
-                           height=256, width=256, num_workers=4, crop=True):
+                         height=256, width=256, num_workers=4, crop=True):
     transform_list = [transforms.ToTensor(),
                       transforms.Normalize((0.5, 0.5, 0.5),
                                            (0.5, 0.5, 0.5))]
-    transform_list = [transforms.RandomCrop((height, width))] + transform_list if crop else transform_list
-    transform_list = [transforms.Resize(new_size)] + transform_list if new_size is not None else transform_list
-    transform_list = [transforms.RandomHorizontalFlip()] + transform_list if train else transform_list
+    transform_list = [transforms.RandomCrop(
+        (height, width))] + transform_list if crop else transform_list
+    transform_list = [transforms.Resize(
+        new_size)] + transform_list if new_size is not None else transform_list
+    transform_list = [transforms.RandomHorizontalFlip()] + \
+        transform_list if train else transform_list
     transform = transforms.Compose(transform_list)
     dataset = ImageFilelist(root, file_list, transform=transform)
-    loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=train, drop_last=True, num_workers=num_workers)
+    loader = DataLoader(dataset=dataset, batch_size=batch_size,
+                        shuffle=train, drop_last=True, num_workers=num_workers)
     return loader
+
 
 def get_data_loader_folder(input_folder, batch_size, train, new_size=None,
                            height=256, width=256, num_workers=4, crop=True):
     transform_list = [transforms.ToTensor(),
                       transforms.Normalize((0.5, 0.5, 0.5),
                                            (0.5, 0.5, 0.5))]
-    transform_list = [transforms.RandomCrop((height, width))] + transform_list if crop else transform_list
-    transform_list = [transforms.Resize(new_size)] + transform_list if new_size is not None else transform_list
-    transform_list = [transforms.RandomHorizontalFlip()] + transform_list if train else transform_list
+    transform_list = [transforms.RandomCrop(
+        (height, width))] + transform_list if crop else transform_list
+    transform_list = [transforms.Resize(
+        new_size)] + transform_list if new_size is not None else transform_list
+    transform_list = [transforms.RandomHorizontalFlip()] + \
+        transform_list if train else transform_list
     transform = transforms.Compose(transform_list)
     dataset = ImageFolder(input_folder, transform=transform)
-    loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=train, drop_last=True, num_workers=num_workers)
+    loader = DataLoader(dataset=dataset, batch_size=batch_size,
+                        shuffle=train, drop_last=True, num_workers=num_workers)
     return loader
 
 
@@ -103,23 +113,28 @@ def get_config(config):
 
 
 def eformat(f, prec):
-    s = "%.*e"%(prec, f)
+    s = "%.*e" % (prec, f)
     mantissa, exp = s.split('e')
     # add 1 to digits as 1 is taken by sign +/-
-    return "%se%d"%(mantissa, int(exp))
+    return "%se%d" % (mantissa, int(exp))
 
 
 def __write_images(image_outputs, display_image_num, file_name):
-    image_outputs = [images.expand(-1, 3, -1, -1) for images in image_outputs] # expand gray-scale images to 3 channels
-    image_tensor = torch.cat([images[:display_image_num] for images in image_outputs], 0)
-    image_grid = vutils.make_grid(image_tensor.data, nrow=display_image_num, padding=0, normalize=True)
+    # expand gray-scale images to 3 channels
+    image_outputs = [images.expand(-1, 3, -1, -1) for images in image_outputs]
+    image_tensor = torch.cat([images[:display_image_num]
+                              for images in image_outputs], 0)
+    image_grid = vutils.make_grid(
+        image_tensor.data, nrow=display_image_num, padding=0, normalize=True)
     vutils.save_image(image_grid, file_name, nrow=1)
 
 
 def write_2images(image_outputs, display_image_num, image_directory, postfix):
     n = len(image_outputs)
-    __write_images(image_outputs[0:n//2], display_image_num, '%s/gen_a2b_%s.jpg' % (image_directory, postfix))
-    __write_images(image_outputs[n//2:n], display_image_num, '%s/gen_b2a_%s.jpg' % (image_directory, postfix))
+    __write_images(image_outputs[0:n//2], display_image_num,
+                   '%s/gen_a2b_%s.jpg' % (image_directory, postfix))
+    __write_images(image_outputs[n//2:n], display_image_num,
+                   '%s/gen_b2a_%s.jpg' % (image_directory, postfix))
 
 
 def prepare_sub_folder(output_directory):
@@ -135,7 +150,8 @@ def prepare_sub_folder(output_directory):
 
 
 def write_one_row_html(html_file, iterations, img_filename, all_size):
-    html_file.write("<h3>iteration [%d] (%s)</h3>" % (iterations,img_filename.split('/')[-1]))
+    html_file.write("<h3>iteration [%d] (%s)</h3>" %
+                    (iterations, img_filename.split('/')[-1]))
     html_file.write("""
         <p><a href="%s">
           <img src="%s" style="width:%dpx">
@@ -157,20 +173,26 @@ def write_html(filename, iterations, image_save_iterations, image_directory, all
     <body>
     ''' % os.path.basename(filename))
     html_file.write("<h3>current</h3>")
-    write_one_row_html(html_file, iterations, '%s/gen_a2b_train_current.jpg' % (image_directory), all_size)
-    write_one_row_html(html_file, iterations, '%s/gen_b2a_train_current.jpg' % (image_directory), all_size)
+    write_one_row_html(html_file, iterations,
+                       '%s/gen_a2b_train_current.jpg' % (image_directory), all_size)
+    write_one_row_html(html_file, iterations,
+                       '%s/gen_b2a_train_current.jpg' % (image_directory), all_size)
     for j in range(iterations, image_save_iterations-1, -1):
         if j % image_save_iterations == 0:
-            write_one_row_html(html_file, j, '%s/gen_a2b_test_%08d.jpg' % (image_directory, j), all_size)
-            write_one_row_html(html_file, j, '%s/gen_b2a_test_%08d.jpg' % (image_directory, j), all_size)
-            write_one_row_html(html_file, j, '%s/gen_a2b_train_%08d.jpg' % (image_directory, j), all_size)
-            write_one_row_html(html_file, j, '%s/gen_b2a_train_%08d.jpg' % (image_directory, j), all_size)
+            write_one_row_html(
+                html_file, j, '%s/gen_a2b_test_%08d.jpg' % (image_directory, j), all_size)
+            write_one_row_html(
+                html_file, j, '%s/gen_b2a_test_%08d.jpg' % (image_directory, j), all_size)
+            write_one_row_html(
+                html_file, j, '%s/gen_a2b_train_%08d.jpg' % (image_directory, j), all_size)
+            write_one_row_html(
+                html_file, j, '%s/gen_b2a_train_%08d.jpg' % (image_directory, j), all_size)
     html_file.write("</body></html>")
     html_file.close()
 
 
 def write_loss(iterations, trainer, train_writer):
-    members = [attr for attr in dir(trainer) \
+    members = [attr for attr in dir(trainer)
                if not callable(getattr(trainer, attr)) and not attr.startswith("__") and ('loss' in attr or 'grad' in attr or 'nwd' in attr)]
     for m in members:
         train_writer.add_scalar(m, getattr(trainer, m), iterations + 1)
@@ -182,7 +204,8 @@ def slerp(val, low, high):
     https://arxiv.org/abs/1609.04468
     Code: https://github.com/soumith/dcgan.torch/issues/14, Tom White
     """
-    omega = np.arccos(np.dot(low / np.linalg.norm(low), high / np.linalg.norm(high)))
+    omega = np.arccos(np.dot(low / np.linalg.norm(low),
+                             high / np.linalg.norm(high)))
     so = np.sin(omega)
     return np.sin((1.0 - val) * omega) / so * low + np.sin(val * omega) / so * high
 
@@ -224,7 +247,8 @@ def load_vgg16(model_dir):
         os.mkdir(model_dir)
     if not os.path.exists(os.path.join(model_dir, 'vgg16.weight')):
         if not os.path.exists(os.path.join(model_dir, 'vgg16.t7')):
-            os.system('wget https://www.dropbox.com/s/76l3rt4kyi3s8x7/vgg16.t7?dl=1 -O ' + os.path.join(model_dir, 'vgg16.t7'))
+            os.system('wget https://www.dropbox.com/s/76l3rt4kyi3s8x7/vgg16.t7?dl=1 -O ' +
+                      os.path.join(model_dir, 'vgg16.t7'))
         vgglua = load_lua(os.path.join(model_dir, 'vgg16.t7'))
         vgg = Vgg16()
         for (src, dst) in zip(vgglua.parameters()[0], vgg.parameters()):
@@ -233,6 +257,7 @@ def load_vgg16(model_dir):
     vgg = Vgg16()
     vgg.load_state_dict(torch.load(os.path.join(model_dir, 'vgg16.weight')))
     return vgg
+
 
 def load_inception(model_path):
     state_dict = torch.load(model_path)
@@ -245,25 +270,29 @@ def load_inception(model_path):
         param.requires_grad = False
     return model
 
+
 def vgg_preprocess(batch):
     tensortype = type(batch.data)
-    (r, g, b) = torch.chunk(batch, 3, dim = 1)
-    batch = torch.cat((b, g, r), dim = 1) # convert RGB to BGR
-    batch = (batch + 1) * 255 * 0.5 # [-1, 1] -> [0, 255]
+    (r, g, b) = torch.chunk(batch, 3, dim=1)
+    batch = torch.cat((b, g, r), dim=1)  # convert RGB to BGR
+    batch = (batch + 1) * 255 * 0.5  # [-1, 1] -> [0, 255]
     mean = tensortype(batch.data.size()).cuda()
     mean[:, 0, :, :] = 103.939
     mean[:, 1, :, :] = 116.779
     mean[:, 2, :, :] = 123.680
-    batch = batch.sub(Variable(mean)) # subtract mean
+    batch = batch.sub(Variable(mean))  # subtract mean
     return batch
 
 
 def get_scheduler(optimizer, hyperparameters, iterations=-1):
     if 'lr_policy' not in hyperparameters or hyperparameters['lr_policy'] == 'constant':
-        scheduler = None # constant scheduler
+        scheduler = None  # constant scheduler
     elif hyperparameters['lr_policy'] == 'step':
         scheduler = lr_scheduler.StepLR(optimizer, step_size=hyperparameters['step_size'],
                                         gamma=hyperparameters['gamma'], last_epoch=iterations)
+    elif hyperparameters['lr_policy'] == 'reduce':
+        scheduler = lr_scheduler.ReduceLROnPlateau(
+            optimizer, 'min', patience=hyperparameters['patience'], factor=hyperparameters['factor'], verbose=True)
     else:
         return NotImplementedError('learning rate policy [%s] is not implemented', hyperparameters['lr_policy'])
     return scheduler
